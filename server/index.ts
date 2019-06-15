152f8de8f12727f70b49528cd1f15593c93d58fc
Cory Reed <swashcap@gmail.com>
2019-06-15 16:53:16 -0700

    feat: add simple Koa server

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import Koa from 'koa';
+import koaLogger from 'koa-logger';
+import koaCors from '@koa/cors';
+import koaRoute from 'koa-route';
+
+const PORT = process.env.PORT || 3000;
+
+export const app = new Koa();
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(koaLogger());
+}
+
+app.use(
+  koaRoute.get('/', ctx => {
+    ctx.body = 'Hello, world!';
+  })
+);
+app.use(koaCors);
+
+if (require.main === module) {
+  app.listen(PORT);
+}
